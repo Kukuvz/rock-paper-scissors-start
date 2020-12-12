d3f17cdd666311eb53272c8d2599b009cec05c27
Kukuvz <antonslayer@gmail.com>
2020-12-12 12:40:43 +0000

    123

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-user_choose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n")) 
 rock = '''
     _______
 ---'   ____)
@@ -26,36 +24,38 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+import random
+user_choise = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n")) 
 # print(type(user_choose))
-computer_choose = random.randint(0, 2)
-if computer_choose == 0:
-  final_computer_choose = rock
-elif computer_choose == 1:
-  final_computer_choose = paper
-elif computer_choose == 2:
-  final_computer_choose = scissors
+computer_choise = random.randint(0, 2)
+if computer_choise == 0:
+  final_computer_choise = rock
+elif computer_choise == 1:
+  final_computer_choise = paper
+elif computer_choise == 2:
+  final_computer_choise = scissors
 
-if user_choose == 0:
-  final_user_choose = rock
-  if final_computer_choose == paper:
+if user_choise == 0:
+  final_user_choise = rock
+  if final_computer_choise == paper:
     result = "You lose"
-  elif final_computer_choose == scissors:
+  elif final_computer_choise == scissors:
     result = "You Win"
   else:
     result = "Draw" 
-elif user_choose == 1:
-  final_user_choose = paper
-  if final_computer_choose == paper:
+elif user_choise == 1:
+  final_user_choise = paper
+  if final_computer_choise == paper:
     result = "Draw"
-  elif final_computer_choose == scissors:
+  elif final_computer_choise == scissors:
     result = "You lose"
   else:
     result = "You Win"
-elif user_choose == 2:
-  final_user_choose = scissors
-  if final_computer_choose == paper:
+elif user_choise == 2:
+  final_user_choise = scissors
+  if final_computer_choise == paper:
     result = "You Win"
-  elif final_computer_choose == scissors:
+  elif final_computer_choise == scissors:
     result = "Draw"
   else:
     result = "You lose"
@@ -63,7 +63,7 @@ else:
   print("Wrong number!!!")
 
 
-print(final_user_choose)
+print(final_user_choise)
 print("Computer chose:")
-print(final_computer_choose)
+print(final_computer_choise)
 print(result)      
